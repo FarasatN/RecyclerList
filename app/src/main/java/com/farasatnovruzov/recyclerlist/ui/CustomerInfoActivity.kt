bd8e0fadf41a2b963005794b9b469ef7d2084779
FarasatN <ferasetnovruzov1@gmail.com>
2023-05-14 01:53:35 +0400

    success and error page ready

--- app/src/main/java/com/farasatnovruzov/recyclerlist/ui/CustomerInfoActivity.kt
+++ app/src/main/java/com/farasatnovruzov/recyclerlist/ui/CustomerInfoActivity.kt
@@ -1,12 +1,28 @@
 package com.farasatnovruzov.recyclerlist.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.farasatnovruzov.recyclerlist.R
 
 class CustomerInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_info)
+
+        val customerInfoImg = findViewById<ConstraintLayout>(R.id.customerInfoImg)
+        val clientTypeCard = findViewById<CardView>(R.id.clientTypeCard)
+
+        customerInfoImg.setOnClickListener {
+            val intent = Intent(this, SuccessActivity::class.java)
+            startActivity(intent)
+        }
+
+        clientTypeCard.setOnClickListener {
+            val intent = Intent(this, ErrorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
