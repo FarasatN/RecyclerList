c3ab539e9a8fbf8dbf6255f8445923ee2652d56f
FarasatN <ferasetnovruzov1@gmail.com>
2023-06-18 14:17:16 +0400

    text watcher added

--- app/src/main/java/com/farasatnovruzov/recyclerlist/ui/EditEmailActivity.kt
+++ app/src/main/java/com/farasatnovruzov/recyclerlist/ui/EditEmailActivity.kt
@@ -1,7 +1,12 @@
 package com.farasatnovruzov.recyclerlist.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextClock
 import com.farasatnovruzov.recyclerlist.BaseActivity
 import com.farasatnovruzov.recyclerlist.R
 
@@ -9,5 +14,6 @@ class EditEmailActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_email)
+
     }
 }
--- app/src/main/java/com/farasatnovruzov/recyclerlist/ui/OTPActivity.kt
+++ app/src/main/java/com/farasatnovruzov/recyclerlist/ui/OTPActivity.kt
@@ -1,8 +1,12 @@
 package com.farasatnovruzov.recyclerlist.ui
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.farasatnovruzov.recyclerlist.BaseActivity
 import com.farasatnovruzov.recyclerlist.R
@@ -17,5 +21,22 @@ class OTPActivity : BaseActivity() {
         val imm: InputMethodManager =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.showSoftInput(pinCodeView, InputMethodManager.SHOW_IMPLICIT)
+
+        pinCodeView.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (pinCodeView.text?.isNotEmpty()==true && pinCodeView.text?.length==6){
+                    val intent = Intent(this@OTPActivity, SecurityActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        })
     }
 }
